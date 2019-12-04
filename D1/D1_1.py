ed4babbf1b82141c93ed621db09efd173d6f1df2
thms <ThmsP@users.noreply.github.com>
2019-12-04 15:04:28 +0100

    Day1 part 2 **

--- D1/D1_1.py
+++ D1/D1_1.py
@@ -15,12 +15,34 @@ def fuel_req(mass):
   """
   return (mass/3)-2
 
+def fuel_fuel(fuel):
+  """
+  Calculate fuel required for fuel
+  Exemple : 
+
+  >>> fuel_fuel(2)
+  0
+  >>> fuel_fuel(654)
+  312
+  >>> fuel_fuel(33583)
+  16763
+  """
+  res = fuel
+  ff = 0
+  while res > 0 :
+    res = fuel_req(res)
+    if res > 0 :
+      ff += res
+  return ff
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
 
     fuel_mod = 0
+    fuel_tot = 0
     with open('input.data','r') as f :
       for m in f.readlines():
-        fuel_mod += fuel_req(int(m))
-    print fuel_mod
+        fuel_mod = fuel_req(int(m))
+        fuel_tot += fuel_mod + fuel_fuel(fuel_mod)
+    print fuel_tot
