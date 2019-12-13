b18d43cbef87e1058cd2c4c78e5b037ad3b7fa92
ThmsP <ThmsP@users.noreply.github.com>
2019-12-13 17:09:41 +0100

    D12 Part2 multprocessing

--- D12/D12.py
+++ D12/D12.py
@@ -2,6 +2,7 @@
 # import logging, sys
 # import math
 import itertools
+import multiprocessing
 
 # logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
@@ -23,81 +24,56 @@ def load_moon_map():
 
   return moons
 
-def add_dico(val, dic, ind):
-  try :
-    dic[ind].append(val)
-  except :
-    dic[ind] = [val]
-  return dic
-
-def apply_gravity(m1, m2):
-  for ind in range(3):
-    if m1[0][ind] < m2[0][ind]:
-      v1 = 1
-      v2 = -1
-    elif m1[0][ind] > m2[0][ind]:
-      v1 = -1
-      v2 = 1
-    else:
-      v1 = v2 = 0
-    m1[1][ind] += v1
-    m2[1][ind] += v2
+def apply_gravity(m1, m2, ind):
+  # for ind in range(3):
+  if m1[0][ind] < m2[0][ind]:
+    v1 = 1
+    v2 = -1
+  elif m1[0][ind] > m2[0][ind]:
+    v1 = -1
+    v2 = 1
+  else:
+    v1 = v2 = 0
+  m1[1][ind] += v1
+  m2[1][ind] += v2
   return m1, m2
 
-def apply_velocity(ms):
+def apply_velocity(ms, ind):
   hist = ''
   for m in ms:
-    for ind in range(3):
-      ms[m][0][ind] += ms[m][1][ind]
-    hist += ''.join(map(str,ms[m][0]))+''.join(map(str,ms[m][1]))
+    ms[m][0][ind] += ms[m][1][ind]
+    hist += str(ms[m][0][ind])+str(ms[m][1][ind])
   return ms, hist
 
-def mem_pos(moons):
+def mem_pos(ms, ind):
   hist = ''
-  # print moons
-  for m in moons:
-    for c in moons[m]:
-      for cc in c:
-        # print cc
-        hist+=str(cc)
-  # print hist
+  for m in ms:
+    hist += str(ms[m][0][ind])+str(ms[m][1][ind])
+
   return hist
 
 
-#pythran export iterations(((int, int, int) list,(int, int, int) list) : int dict, int)
-def iterations(moons, ite=0):
-  # moons_couple = [i for i in itertools.permutations(moons,2)]
-  moons_cpl_iterator = itertools.permutations(moons,2)
-  moons_couple = []
-  for i in moons_cpl_iterator:
-    if i not in moons_couple and (i[1],i[0]) not in moons_couple:
-      moons_couple.append(i)
+#pythran export iterations(((int, int, int) list,(int, int, int) list) list : str dict, int)
+def iterations(ind):
 
-  # print moons_couple
+  moons = load_moon_map()
+  moons_couple = [i for i in itertools.combinations(moons,2)]
 
-  moons_pos = mem_pos(moons)
-  ite =1
-  while True:
-    if not ite % 10000:
-      # logging.info('ieration %i ', ite)
-      print 'ite %i'%ite
-    # for m in moons:
-      # logging.info("moon %s %s",m, moons[m])
+
+  moons_pos = mem_pos(moons, ind)
+  found = []
+  while len(found)<2000:
+
     for mc in moons_couple:
-      moons[mc[0]], moons[mc[1]] = apply_gravity(moons[mc[0]], moons[mc[1]])
-    # for m in moons:
-    moons, pos = apply_velocity(moons)
-    # pos = mem_pos(moons)
-    # print pos
-    if pos in moons_pos:
-      # logging.info('Universe repeat ! %i', ite)
-      print 'Universe repeat ! %i '%ite
-      break
-    else:
-      moons_pos+=pos
-    ite += 1
+      moons[mc[0]], moons[mc[1]] = apply_gravity(moons[mc[0]], moons[mc[1]], ind)
+    moons, pos = apply_velocity(moons, ind)
 
-  return moons
+    if pos in moons_pos:
+      found.append(ite)
+      print len(found)
+
+
+  return found
 
 # def potential_energy(m):
 #   ep = 0
@@ -127,15 +103,18 @@ def iterations(moons, ite=0):
 
 
 
-
-# if __name__ == "__main__":
-  # import doctest
-  # doctest.testmod()
-
-  # moons = iterations(load_moon_map(), 1000)  
-  # system_energy = total_energy(moons)
-  # logging.info('total energy of the system : %i', system_energy)
-
+# 
+if __name__ == "__main__":
+  pool = multiprocessing.Pool(8)
+  results = []
+  for x in pool.imap_unordered(iterations, range(3)):
+    results.append(x)
+    pass
+  pool.close()
+  pool.join()
+  min_ite_per_axis = set(results[0]) & set(results[1]) & set(results[2])
+  min_ite          = min(min_ite_per_axis)
+  print "FINAL FOUND : %i"%min_ite
 
 
   
